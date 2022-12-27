15c078d911edb949c66cf7243236d1a953d7440e
Varinder Singh <varinder.singh01@nagarro.com>
2022-12-27 14:00:43 +0530

    remove eureka dependency

--- src/main/java/com/nagarro/training/microservice/DbProductServiceApplication.java
+++ src/main/java/com/nagarro/training/microservice/DbProductServiceApplication.java
@@ -2,10 +2,8 @@ package com.nagarro.training.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class DbProductServiceApplication {
 
 	public static void main(String[] args) {
